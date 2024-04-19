c12e39f275082aa6a26c61989eeece37ecac9ff6
Ike-Martha <amagwen36@gmail.com>
2024-04-19 10:55:01 +0100

    Add project files.

---
+++ StudentGrade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsGrade
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
